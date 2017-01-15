d5606fa21692ba2871a6fbb7f1b6b8e12315913e
Siegfried Gündert <siegfried.guendert@googlemail.com>
2017-01-15 13:18:22 +0100

    Adds second example with SoundFileSource.

---
+++ examples/example2.py
@@ -0,0 +1,26 @@
+from sigfeat.source import SoundFileSource
+from sigfeat.preprocess import MeanMix
+from sigfeat import features as fts
+from sigfeat.sink import DefaultDictSink
+from sigfeat.extractor import Extractor
+
+
+extractor = Extractor(
+    fts.SpectralFlux(),
+    fts.SpectralCentroid(),
+    fts.SpectralFlatness(),
+)
+
+
+if __name__ == '__main__':
+    from pylab import plt, np
+
+    src = MeanMix(SoundFileSource('bonsai.wav'))
+    sink = DefaultDictSink()
+    extractor.extract(src, sink)
+
+    plt.figure(src.name)
+    for l, r in sink['results'].items():
+        plt.plot(r/np.max(np.abs(r)), label=str(l))
+    plt.legend()
+    plt.show()

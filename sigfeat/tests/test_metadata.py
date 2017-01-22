93eec2ecc26b5a74ff35a50de0c1b2a152017c66
Siegfried Gündert <siegfried.guendert@googlemail.com>
2017-01-22 17:26:33 +0100

    Adds metadata tests.

---
+++ sigfeat/tests/test_metadata.py
@@ -0,0 +1,35 @@
+import pytest
+from sigfeat._metadata import MetadataMixin
+
+
+class MetadataMixinSubclass(MetadataMixin):
+
+    def __init__(self):
+        self.fetch_metadata_as_attrs()
+
+
+def test_add_metadata():
+    s = MetadataMixinSubclass()
+    s.add_metadata('bum', 1)
+    s.add_metadata('eins', 'Bum')
+    assert s.metadata[-1] == ('eins', 'Bum')
+
+
+def test_extend_metadata():
+    s = MetadataMixinSubclass()
+    s.extend_metadata((
+        ('a', 'b'),
+        ('c', 'd')
+    ))
+    assert len(s.metadata) == 3
+
+
+def test_fetch_metadata_as_attributes():
+    s = MetadataMixinSubclass()
+    s.add_metadata('test', 'test')
+    s.fetch_metadata_as_attrs()
+    assert s.test == 'test'
+
+
+if __name__ == '__main__':
+    pytest.main()

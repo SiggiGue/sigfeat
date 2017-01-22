aecc8ba7bda1dee65f8e5a2c5f8117c30a23d073
Siegfried Gündert <siegfried.guendert@googlemail.com>
2017-01-22 17:15:26 +0100

    Adds result tests.

---
+++ sigfeat/tests/test_result.py
@@ -0,0 +1,20 @@
+import pytest
+from sigfeat.result import Result
+
+
+def test_result_creation():
+    res = Result()
+    assert isinstance(res, Result)
+
+
+def test_result_setitem():
+    r = Result()
+    with pytest.raises(TypeError):
+        r['test'] = 'Test'
+
+    r._setitem('test', 'Test')
+    assert r['test'] == 'Test'
+
+
+if __name__ == '__main__':
+    pytest.main()

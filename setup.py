10548d3bd762c40f0108bbc255a3331d6dcab99b
Siegfried Gündert <siegfried.guendert@googlemail.com>
2017-02-03 10:37:16 +0100

    Adds test to setup and again adapt coverage command.

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 # -- encoding: utf-8 --
-
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(name='sigfeat',
       version='0.2alpha',
@@ -9,4 +24,6 @@ setup(name='sigfeat',
       author_email='siegfried.guendert@googlemail.com',
       license='',
       packages=find_packages(exclude=('docs', '.git', '__pycache__')),
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
       )
